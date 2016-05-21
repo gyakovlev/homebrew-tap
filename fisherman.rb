cc8758e9caa925596881127974bd3ff003a371b9
Jorge Bucaran <j@bucaran.me>
2016-05-22 03:49:27 +0900

    Add completions and improve install log.
    * Create vendor_completions.d/fisher.fish.
    
    * Warn the user the terminal / session may
      need to be reset for changes to take effect.

--- fisherman.rb
+++ fisherman.rb
@@ -1,67 +1,73 @@
 class Fisherman < Formula
-  desc "Fish plugin manager"
-  homepage "https://github.com/fisherman/fisherman"
+    desc "fish plugin manager"
+    homepage "https://github.com/fisherman/fisherman"
 
-  url "https://raw.githubusercontent.com/fisherman/fisherman/2.7.8/fisher.fish"
-  sha256 "72ad19b140183175540c5eae34cea6c362accb62e3057e86e26b47494661f7b3"
+    url "https://raw.githubusercontent.com/fisherman/fisherman/2.7.9/fisher.fish"
+    sha256 "e75d89d60b572aa96648d444ea0d7fbb54560af40e5b4ffd56ffb1e3df7db6b3"
 
-  head "https://github.com/fisherman/fisherman.git"
+    head "https://github.com/fisherman/fisherman.git"
 
-  depends_on "fish"
+    depends_on "fish"
 
+    require 'open3'
 
-  require 'open3'
-
-  def fish_v
-    out, st = Open3.capture2e("fish -v")
-    if st.exitstatus != 0
-      odie "Fish is not found!"
-    else
-      out.strip
+    def fish_v
+        out, st = Open3.capture2e("fish -v")
+        if st.exitstatus != 0
+            odie "fish-shell not installed."
+        else
+            out.strip
+        end
     end
-  end
 
-  def fish_2_2?
-    fish_v.start_with?("fish, version 2.2")
-  end
+    def fish_2_2?
+        fish_v.start_with?("fish, version 2.2")
+    end
 
+    def install
+        share.install "fisher.fish"
 
-  def install
-    share.install "fisher.fish"
+        fish_prefix = File.realpath("#{HOMEBREW_PREFIX}/opt/fish/")
 
-    fish_prefix = File.realpath("#{HOMEBREW_PREFIX}/opt/fish/")
-
-    destination =
-      if fish_2_2?
-        "#{fish_prefix}/share/fish/functions"
-      else
-        "#{fish_prefix}/share/fish/vendor_functions.d"
-      end
-
-    ohai "fisher function will live in \n #{destination}"
-
-    makedirs destination
-    ln_sf "#{share}/fisher.fish", "#{destination}/fisher.fish"
-  end
-
-
-  def caveats
-    if fish_2_2?
-      <<-EOS.undent
-        It seems that you are running fish 2.2. For snippet support, upgrade to >=2.3 or
-        append the following code to your ~/.config/fish/config.fish:
-
-            for file in ~/.config/fish/conf.d/*.fish
-                source $file
+        functions_dest =
+            if fish_2_2?
+                "#{fish_prefix}/share/fish/functions"
+            else
+                "#{fish_prefix}/share/fish/vendor_functions.d"
             end
-      EOS
+
+        completions_dest =
+            if fish_2_2?
+                "#{fish_prefix}/share/fish/completions"
+            else
+                "#{fish_prefix}/share/fish/vendor_completions.d"
+            end
+
+        ohai "fisherman will be linked to:"
+        ohai "#{functions_dest}"
+        ohai "#{completions_dest}\n"
+
+        ohai "You may need to restart any open terminal sessions for changes to take effect."
+
+        makedirs functions_dest
+        makedirs completions_dest
+
+        File.write("#{share}/fisher-complete.fish", "fisher --complete")
+
+        ln_sf "#{share}/fisher.fish", "#{functions_dest}/fisher.fish"
+        ln_sf "#{share}/fisher-complete.fish", "#{completions_dest}/fisher.fish"
     end
-  end
 
+    def caveats
+        if fish_2_2?
+            <<-EOS.undent
+                It seems that you are running fish 2.2. For snippet support, upgrade to >=2.3 or
+                append the following code to your ~/.config/fish/config.fish:
 
-  test do
-    system "fish", "-c", "fisher -v"
-    # tried to check here, that the output version is the same as in the formula
-    # but it doesn't make sense with HEAD
-  end
+                for file in ~/.config/fish/conf.d/*.fish
+                    source $file
+                end
+            EOS
+        end
+    end
 end
